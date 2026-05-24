9449aa62a167aa052ddb19ce33811df4834630f2
Arka Patra <arkapatra489@gmail.com>
2026-05-24 16:33:50 +0530

    Fixed CORS and PORT

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,14 @@ const analyzeRoutes = require("./routes/analyze.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
+
 app.use(express.json());
 
 app.use("/api/analyze", analyzeRoutes);
 
-// IMPORTANT FOR RENDER
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
